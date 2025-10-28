cd20fe7b56efbf9dc917586f2905884333593698
MuriloGJ <januario.eu@gmail.com>
2025-10-28 01:51:26 -0300

    sobre

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -7,7 +7,7 @@ namespace MauiAppHotel
         public App()
         {
             InitializeComponent();
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.Sobre());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -13,6 +13,13 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("BodoniModa-Italic.ttf", "BodoniModa-Italic");
+                    fonts.AddFont("BodoniModa-VariableFont.ttf", "BodoniModa");
+                    fonts.AddFont("Domine-Bold.ttf", "Domine-Bold");
+                    fonts.AddFont("Domine-Medium.ttf", "Domine-Medium");
+                    fonts.AddFont("Domine-Regular.ttf", "Domine-Regular");
+                    fonts.AddFont("Domine-SemiBold.ttf", "Domine-SemiBold");
+
                 });
 
 #if DEBUG
